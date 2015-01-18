4a42ca1fa10cdb20bb4631f708e7779a445c9349
Michael Lesniak <mail@mlesniak.com>
2015-01-18 19:09:34 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Exception: Caused by: org.apache.hadoop.ipc.RemoteException: Permission denied
+
+To access the HDFS-filesystem of the cluster, the current user $USER has to have an account on the namenode and
+write permissions to the directory. For the Cloudera-VM this means (for $USER == mlesniak)
+
+    sudo adduser mlesniak
+    sudo usermod -a -G cloudera mlesniak
+    hadoop fs -chmod g+w /user/cloudera
+
---
+++ src/main/java/com/mlesniak/data/in/Main.java
@@ -0,0 +1,58 @@
+package com.mlesniak.data.in;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    private Logger log = LoggerFactory.getLogger(Main.class);
+
+    @Autowired
+    private Environment env;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Application started");
+
+        String hdfsServer = env.getProperty("hdfsServer");
+        String fileName = env.getProperty("fileName");
+        String content = env.getProperty("content");
+
+        Configuration configuration = new Configuration();
+        log.info("Connection to {}", hdfsServer);
+        FileSystem hdfs = FileSystem.get(new URI(hdfsServer), configuration);
+        Path file = new Path(hdfsServer + "/" + fileName);
+
+        // Remove file if already existing (for testing purposes).
+        log.info("Filename: {}", fileName);
+        if (hdfs.exists(file)) {
+            log.info("Removing file");
+            hdfs.delete(file, true);
+        }
+
+        log.info("Creating file");
+        OutputStream os = hdfs.create(file);
+        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+        br.write(content);
+        br.close();
+        hdfs.close();
+        log.info("Bye");
+    }
+}
